1828bdc6471ab935faf12ebd6db27850a18aca69
olegpetrenko <gg.jugger@gmail.com>
2017-09-20 23:02:04 +0300

    typo fix

--- app/assets/javascripts/game/init.js
+++ app/assets/javascripts/game/init.js
@@ -8,68 +8,68 @@ var bulletTime = 0;
 var fireButton;
 
 function preload() {
-	game.load.image('bg', 'assets/images/background/bg.jpg');
-	game.load.image('forest_gr_center', 'assets/images/background/forest/WaveForest_Square.png');
-    game.load.image('forest_gr_bottom', 'assets/images/background/forest/WaveForest_MudSquare.png');
-	game.load.spritesheet('unit', 'assets/images/units/player1.png', 150, 200);
-	game.load.image('bullet1', 'assets/images/items/bullet1.png');
-	game.load.tilemap('level1_tile', 'assets/tiles/level1.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.image('bg', 'assets/images/background/bg.jpg');
+  game.load.image('forest_gr_center', 'assets/images/background/forest/WaveForest_Square.png');
+  game.load.image('forest_gr_bottom', 'assets/images/background/forest/WaveForest_MudSquare.png');
+  game.load.spritesheet('unit', 'assets/images/units/player1.png', 150, 200);
+  game.load.image('bullet1', 'assets/images/items/bullet1.png');
+  game.load.tilemap('level1_tile', 'assets/tiles/level1.json', null, Phaser.Tilemap.TILED_JSON);
 }
 
 function create() {
-	game.physics.startSystem(Phaser.Physics.ARCADE);
-	bg = game.add.sprite(0, 0, 'bg');
-    bg.fixedToCamera = true;
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  bg = game.add.sprite(0, 0, 'bg');
+  bg.fixedToCamera = true;
 
-	var map = game.add.tilemap('level1_tile');
-	map.addTilesetImage('forest_ground_center', 'forest_gr_center');
-    map.addTilesetImage('forest_ground_bottom', 'forest_gr_bottom');
+  var map = game.add.tilemap('level1_tile');
+  map.addTilesetImage('forest_ground_center', 'forest_gr_center');
+  map.addTilesetImage('forest_ground_bottom', 'forest_gr_bottom');
 
-	var backgroundlayer = map.createLayer('BackgroundLayer');
-	groundLayer = map.createLayer('GroundLayer');
+  var backgroundlayer = map.createLayer('BackgroundLayer');
+  groundLayer = map.createLayer('GroundLayer');
 
-	map.setCollisionBetween(1, 100, true, groundLayer);
+  map.setCollisionBetween(1, 100, true, groundLayer);
 
-    //Change the world size to match the size of this layer
-    groundLayer.resizeWorld();
+  //Change the world size to match the size of this layer
+  groundLayer.resizeWorld();
 
-    player = new Player(game, 'unit')
-    player.create(150, 100);
+  player = new Player(game, 'unit')
+  player.create(150, 100);
 
-    var weapon = new Weapon(game, 'bullet1');
-    weapon.create(40);
+  var weapon = new Weapon(game, 'bullet1');
+  weapon.create(40);
 
-    player.set_weapon(weapon);
+  player.set_weapon(weapon);
 
-    cursors = game.input.keyboard.createCursorKeys();
-    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.X);
+  cursors = game.input.keyboard.createCursorKeys();
+  jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  fireButton = game.input.keyboard.addKey(Phaser.Keyboard.X);
 }
 
 function update() {
-	game.physics.arcade.collide(player.unit, groundLayer);
+  game.physics.arcade.collide(player.unit, groundLayer);
 
-	player.nullify_velocity();
+  player.nullify_velocity();
 
-    if (cursors.left.isDown)
-    {
-        player.move_left();
-    }
-    else if (cursors.right.isDown)
-    {
-        player.move_right();
-    }
-    else {
-        player.stop();
-    }
+  if (cursors.left.isDown)
+  {
+    player.move_left();
+  }
+  else if (cursors.right.isDown)
+  {
+    player.move_right();
+  }
+  else {
+    player.stop();
+  }
 
-    if (fireButton.isDown)
-    {
-        player.fire();
-    }
+  if (fireButton.isDown)
+  {
+    player.fire();
+  }
 
-    if (jumpButton.isDown && player.on_floor() && game.time.now > player.jumpTimer)
-    {
-        player.jump();
-    }
+  if (jumpButton.isDown && player.on_floor() && game.time.now > player.jumpTimer)
+  {
+    player.jump();
+  }
 }
--- app/assets/javascripts/game/models/player.js
+++ app/assets/javascripts/game/models/player.js
@@ -1,8 +1,8 @@
 var Player = function(game, sprite) {
-	this.game = game;
-	this.sprite = sprite;
-	this.unit;
-	this.jumpTimer = 0;
+  this.game = game;
+  this.sprite = sprite;
+  this.unit;
+  this.jumpTimer = 0;
 }
 
 Player.DEFAULT_MOVE_SPEED = 200;
@@ -10,10 +10,10 @@ Player.DEFAULT_JUMP_SPEED = 350;
 Player.DEFAULT_FACING = 'right';
 
 Player.prototype.create = function(positionX, positionY) {
-	this.unit = this.game.add.sprite(positionX, positionY, this.sprite);
-	this.game.physics.enable(this.unit, Phaser.Physics.ARCADE);
-	this.unit.body.collideWorldBounds = true;
-	this.unit.body.bounce.y = 0.2;
+  this.unit = this.game.add.sprite(positionX, positionY, this.sprite);
+  this.game.physics.enable(this.unit, Phaser.Physics.ARCADE);
+  this.unit.body.collideWorldBounds = true;
+  this.unit.body.bounce.y = 0.2;
   this.unit.body.gravity.y = DEFAULT_GRAVITY;
   this.facing = Player.DEFAULT_FACING;
   this.unit.animations.add('right', [1, 2, 3, 4, 5, 6, 7, 8], 10, true);
@@ -21,19 +21,19 @@ Player.prototype.create = function(positionX, positionY) {
   this.unit.animations.add('left', [16, 15, 14, 13, 12, 11, 10, 9], 10, true);
   this.unit.body.setSize(80, 185, 15, 0);
   this.game.camera.follow(this.unit);
-	return this.unit;
+  return this.unit;
 }
 
 Player.prototype.x = function() {
-	return this.unit.x;
+  return this.unit.x;
 }
 
 Player.prototype.y = function() {
-	return this.unit.y;
+  return this.unit.y;
 }
 
 Player.prototype.on_floor = function() {
-	return this.unit.body.onFloor();
+  return this.unit.body.onFloor();
 }
 
 Player.prototype.set_weapon = function(weapon) {
@@ -42,31 +42,31 @@ Player.prototype.set_weapon = function(weapon) {
 }
 
 Player.prototype.nullify_velocity = function() {
-	this.unit.body.velocity.x = 0;
+  this.unit.body.velocity.x = 0;
 }
 
 Player.prototype.move_left = function() {
-	this.unit.body.velocity.x = - Player.DEFAULT_MOVE_SPEED;
-	this.unit.animations.play('left');
-	this.facing = 'left';
+  this.unit.body.velocity.x = - Player.DEFAULT_MOVE_SPEED;
+  this.unit.animations.play('left');
+  this.facing = 'left';
   this.weapon.set_fire_angle(Phaser.ANGLE_LEFT);
 }
 
 Player.prototype.move_right = function() {
-	this.unit.body.velocity.x = Player.DEFAULT_MOVE_SPEED;
-	this.unit.animations.play('right');
-	this.facing = 'right';
+  this.unit.body.velocity.x = Player.DEFAULT_MOVE_SPEED;
+  this.unit.animations.play('right');
+  this.facing = 'right';
   this.weapon.set_fire_angle(Phaser.ANGLE_RIGHT);
 }
 
 Player.prototype.stop = function() {
-	this.unit.animations.stop();
-	if (this.facing == 'right') {
-		this.unit.frame = 0;
-	}
-	else {
-		this.unit.frame = 17;
-	}
+  this.unit.animations.stop();
+  if (this.facing == 'right') {
+    this.unit.frame = 0;
+  }
+  else {
+    this.unit.frame = 17;
+  }
 }
 
 Player.prototype.fire = function() {
@@ -74,6 +74,6 @@ Player.prototype.fire = function() {
 }
 
 Player.prototype.jump = function() {
-	this.unit.body.velocity.y = - Player.DEFAULT_JUMP_SPEED;
+  this.unit.body.velocity.y = - Player.DEFAULT_JUMP_SPEED;
   this.jumpTimer = this.game.time.now + 750;
 }
